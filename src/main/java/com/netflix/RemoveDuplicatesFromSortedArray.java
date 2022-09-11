5a3d85c19b677537e8f1ee760feb539f48b81aa5
Rostyslav Kholodnytskyi <rostyslav.kholodnytskyi@gmail.com>
2022-09-11 13:07:13 +0200

    add gitignore

---
+++ src/main/java/com/netflix/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,13 @@
+package com.netflix;
+
+public class RemoveDuplicatesFromSortedArray {
+
+    public static int removeDuplicates(int[] nums) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
