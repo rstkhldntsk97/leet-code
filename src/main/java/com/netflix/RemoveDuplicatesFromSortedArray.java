64653fed6cb9109f265859b52f80404de6b94a4f
Rostyslav Kholodnytskyi <rostyslav.kholodnytskyi@gmail.com>
2022-09-11 14:06:31 +0200

    add solution for LC-26

--- src/main/java/com/netflix/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/netflix/RemoveDuplicatesFromSortedArray.java
@@ -3,11 +3,18 @@ package com.netflix;
 public class RemoveDuplicatesFromSortedArray {
 
     public static int removeDuplicates(int[] nums) {
-        return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(removeDuplicates(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4})); // 5, nums = [0,1,2,3,4,_,_,_,_,_]
     }
 
 }
