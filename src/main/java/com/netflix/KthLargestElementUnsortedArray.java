68edb6fe2b5a15eb1ffa06e243424034283dfa5d
Rostyslav Kholodnytskyi <rostyslav.kholodnytskyi@gmail.com>
2022-10-12 10:13:27 +0200

    add solution for LC-215

---
+++ src/main/java/com/netflix/KthLargestElementUnsortedArray.java
@@ -0,0 +1,20 @@
+package com.netflix;
+
+import java.util.PriorityQueue;
+
+public class KthLargestElementUnsortedArray {
+
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k) minHeap.remove();
+        }
+        return minHeap.remove();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findKthLargest(new int[]{7, 1, 0, 5, 6, 3}, 3));
+    }
+
+}
