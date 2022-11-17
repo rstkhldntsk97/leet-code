f327762352526bd6668a733a7f41a29677766676
Rostyslav Kholodnytskyi <rostyslav.kholodnytskyi@gmail.com>
2022-11-17 20:02:56 +0100

    heapsort implementation accordingly to Introduction to Algorithms(Thomas H.Cormen , Charles E.Leiserson, Ronald L.Rivest, Clifford Stein)

--- src/main/java/com/netflix/HeapSort.java
+++ src/main/java/com/netflix/HeapSort.java
@@ -1,26 +1,26 @@
 package com.netflix;
 
-import java.util.Arrays;
-
 public class HeapSort {
 
-    public static void main(String[] args) {
-        var heaP = new HeapSort();
-//        var heap = new int[]{16, 4, 10, 14, 7, 9, 3, 2, 8, 1};
-        var heap = new int[]{4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
-        heaP.buildMaxHeap(heap);
-//        heaP.maxHeapify(heap, 1, heap.length);
-        System.out.println(Arrays.toString(heap));
-    }
-
-    public void buildMaxHeap(int[] arr) {
-        for (int i = arr.length / 2 - 1; i >= 0; i--) {
-            maxHeapify(arr, i);
+    public void heapSort(int[] arr) {
+        int N = arr.length;
+        buildMaxHeap(arr, N);
+        for (int i = N - 1; i >= 0; i --) {
+            var tmp = arr[0];
+            arr[0] = arr[N - 1];
+            arr[N - 1] = tmp;
+            N = N - 1;
+            maxHeapify(arr, 0, N);
         }
     }
 
-    public void maxHeapify(int[] heap, int i) {
-        int N = heap.length;
+    public void buildMaxHeap(int[] arr, int N) {
+        for (int i = N / 2 - 1; i >= 0; i--) {
+            maxHeapify(arr, i, N);
+        }
+    }
+
+    public void maxHeapify(int[] heap, int i, int N) {
         int l = i * 2 + 1;
         int r = i * 2 + 2;
         int largest;
@@ -37,7 +37,7 @@ public class HeapSort {
             int tmp = heap[i];
             heap[i] = heap[largest];
             heap[largest] = tmp;
-            maxHeapify(heap, largest);
+            maxHeapify(heap, largest, N);
         }
     }
 
---
+++ src/test/java/com/netflix/HeapSortTest.java
@@ -0,0 +1,37 @@
+package com.netflix;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HeapSortTest {
+
+    HeapSort main = new HeapSort();
+
+    @Test
+    void heapSort() {
+        var heap = new int[]{16, 14, 10, 8, 7, 9, 3, 2, 4, 1};
+        var heap1 = new int[]{5, 13, 2, 25, 7, 17, 20, 8, 4};
+        main.heapSort(heap);
+        main.heapSort(heap1);
+        assertEquals(Arrays.toString(new int[]{1, 2, 3, 4, 7, 8, 9, 10, 14, 16}), Arrays.toString(heap));
+        assertEquals(Arrays.toString(new int[]{2, 4, 5, 7, 8, 13, 17, 20, 25}), Arrays.toString(heap1));
+    }
+
+    @Test
+    void maxHeapify() {
+        var heap = new int[]{16, 4, 10, 14, 7, 9, 3, 2, 8, 1};
+        main.maxHeapify(heap, 1, heap.length);
+        assertEquals(Arrays.toString(new int[]{16, 14, 10, 8, 7, 9, 3, 2, 4, 1}), Arrays.toString(heap));
+    }
+
+    @Test
+    void buildMaxHeap() {
+        var heap = new int[]{5, 3, 17, 10, 84, 19, 6, 22, 9};
+        main.buildMaxHeap(heap, heap.length);
+        assertEquals(Arrays.toString(new int[]{84, 22, 19, 10, 3, 17, 6, 5, 9}), Arrays.toString(heap));
+    }
+
+}
