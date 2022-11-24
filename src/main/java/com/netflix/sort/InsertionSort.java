687357743c8311aa303e7fb71e7d12bf5d28d49c
Rostyslav Kholodnytskyi <rostyslav.kholodnytskyi@gmail.com>
2022-11-24 23:20:06 +0100

    add insertion sort in decreasing order

--- src/main/java/com/netflix/sort/InsertionSort.java
+++ src/main/java/com/netflix/sort/InsertionSort.java
@@ -15,4 +15,17 @@ public class InsertionSort {
         return arr;
     }
 
+    public int[] decreasingSort(int[] arr) {
+        for (int j = 1; j < arr.length; j++) {
+            int key = arr[j];
+            int i = j - 1;
+            while (i >= 0 && arr[i] < key) {
+                arr[i + 1] = arr[i];
+                i--;
+            }
+            arr[i + 1] = key;
+        }
+        return arr;
+    }
+
 }
--- src/test/java/com/netflix/sort/InsertionSortTest.java
+++ src/test/java/com/netflix/sort/InsertionSortTest.java
@@ -10,7 +10,14 @@ class InsertionSortTest {
     void sort() {
         var main = new InsertionSort();
         assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6}, main.sort(new int[]{5, 2, 4, 6, 1, 3}));
-        assertArrayEquals(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, main.sort(new int[]{5, 3, 2, 1, 6, 8, 4, 7, 9, 0}));
+        assertArrayEquals(new int[]{26, 31, 41, 41, 58, 59}, main.sort(new int[]{31, 41, 59, 26, 41, 58}));
+    }
+
+    @Test
+    void decreasingSort() {
+        var main = new InsertionSort();
+        assertArrayEquals(new int[]{6, 5, 4, 3, 2, 1}, main.decreasingSort(new int[]{5, 2, 4, 6, 1, 3}));
+        assertArrayEquals(new int[]{59, 58, 41, 41, 31, 26}, main.decreasingSort(new int[]{31, 41, 59, 26, 41, 58}));
     }
 
 }
