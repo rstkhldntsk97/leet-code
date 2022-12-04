86021b7a9aa61e74285a373f14d1fd541b7ccb63
Rostyslav Kholodnytskyi <rostyslav.kholodnytskyi@gmail.com>
2022-12-04 15:13:13 +0100

    LC-2256. Minimum Average Difference (Prefix sum impl with O(n))

--- src/main/java/com/leetcode/MinimumAverageDifference.java
+++ src/main/java/com/leetcode/MinimumAverageDifference.java
@@ -1,37 +1,30 @@
 package com.leetcode;
 
-import java.util.Arrays;
-
 public class MinimumAverageDifference {
 
     public int minimumAverageDifference(int[] nums) {
-        int minAvgDiff = Integer.MAX_VALUE;
-        int res = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int curAvgDiff = avgDifference(nums, i);
-            if (minAvgDiff > curAvgDiff) {
-                minAvgDiff = curAvgDiff;
-                res = i;
+        int res = -1;
+        int minDiff = Integer.MAX_VALUE;
+        long currPrefixSum = 0;
+        long totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        for (int index = 0; index < nums.length; index++) {
+            currPrefixSum += nums[index];
+            long leftAvg = currPrefixSum;
+            leftAvg /= (index + 1);
+            long rightAvg = totalSum - currPrefixSum;
+            if (index != nums.length - 1) {
+                rightAvg /= (nums.length - index - 1);
+            }
+            int currDiff = (int) Math.abs(leftAvg - rightAvg);
+            if (currDiff < minDiff) {
+                minDiff = currDiff;
+                res = index;
             }
         }
         return res;
     }
-
-    // input --> 2, 5, 3, 9, 5, 3
-    // idx 0 --> (2 / 1) - (5 + 3 + 9 + 5 + 3) / 5|
-    // idx 1 --> (2 + 5) / 2 - (3 + 9 + 5 + 3) / 4|
-    // idx 2 --> (2 + 5 + 3) / 3 - (9 + 5 + 3) / 3|
-    // idx 3 --> (2 + 5 + 3 + 9) / 4 - (5 + 3) / 2|
-    // idx 4 --> (2 + 5 + 3 + 9 + 5) / 5 - 3 / 1|
-    // idx 5 --> (2 + 5 + 3 + 9 + 5 + 3) / 6 - 0|
-    public int avgDifference(int[] nums, int i) {
-        int[] left = Arrays.copyOfRange(nums, 0, i + 1);
-        int[] right = Arrays.copyOfRange(nums, i + 1, nums.length);
-        int leftSum = Arrays.stream(left).sum();
-        int rightSum = Arrays.stream(right).sum();
-        int leftRes = leftSum / left.length;
-        int rightRes = right.length == 0 ? 0 : rightSum / right.length;
-        return Math.abs(leftRes - rightRes);
-    }
-
 }
--- src/test/java/com/leetcode/MinimumAverageDifferenceTest.java
+++ src/test/java/com/leetcode/MinimumAverageDifferenceTest.java
@@ -11,16 +11,7 @@ class MinimumAverageDifferenceTest {
     @Test
     void minimumAverageDifference() {
         assertEquals(3, main.minimumAverageDifference(new int[]{2, 5, 3, 9, 5, 3}));
-    }
-
-    @Test
-    void avgDifference() {
-        assertEquals(3, main.avgDifference(new int[]{2, 5, 3, 9, 5, 3}, 0));
-        assertEquals(2, main.avgDifference(new int[]{2, 5, 3, 9, 5, 3}, 1));
-        assertEquals(2, main.avgDifference(new int[]{2, 5, 3, 9, 5, 3}, 2));
-        assertEquals(0, main.avgDifference(new int[]{2, 5, 3, 9, 5, 3}, 3));
-        assertEquals(1, main.avgDifference(new int[]{2, 5, 3, 9, 5, 3}, 4));
-        assertEquals(4, main.avgDifference(new int[]{2, 5, 3, 9, 5, 3}, 5));
+        assertEquals(2, main.minimumAverageDifference(new int[]{1, 4, 5, 6, 3, 3, 6, 2, 1, 1}));
     }
 
 }
