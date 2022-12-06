bcba0eeac31fe268e1586fac7b10d57bd72528c1
Rostyslav Kholodnytskyi <rostyslav.kholodnytskyi@gmail.com>
2022-12-06 22:30:03 +0100

    LC-328. Odd Even Linked List(better solution)

--- src/main/java/com/leetcode/OddEvenLinkedList.java
+++ src/main/java/com/leetcode/OddEvenLinkedList.java
@@ -6,31 +6,12 @@ public class OddEvenLinkedList {
 
     public ListNode oddEvenList(ListNode head) {
         if (head == null) return null;
-        if (head.next == null) return null;
-        int counter = 1;
-        var curr = head;
-        ListNode odd = head;
-        ListNode oddCur = odd;
-
-        ListNode even = head.next;
-        ListNode evenCur = even;
-
-        curr = curr.next;
-        while (curr != null) {
-            if (counter % 2 == 0) {
-                oddCur.next = curr;
-                oddCur = oddCur.next;
-            } else if (counter != 1 && counter % 2 == 1) {
-                evenCur.next = curr;
-                evenCur = evenCur.next;
-            }
-            counter++;
-            curr = curr.next;
-        }
-        if (counter % 2 == 0) {
-            oddCur.next = null;
-        } else if (counter % 2 == 1) {
-            evenCur.next = null;
+        ListNode oddCur = head, evenCur = head.next, even = evenCur;
+        while (evenCur != null && evenCur.next != null) {
+            oddCur.next = evenCur.next;
+            oddCur = oddCur.next;
+            evenCur.next = oddCur.next;
+            evenCur = evenCur.next;
         }
         oddCur.next = even;
         return head;
