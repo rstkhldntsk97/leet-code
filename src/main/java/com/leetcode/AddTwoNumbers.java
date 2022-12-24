e0ff43da0cf2cc94760fa89b9f9d07c92a181041
Rostyslav Kholodnytskyi <rostyslav.kholodnytskyi@gmail.com>
2022-12-24 11:08:54 +0100

    LC-2. Add Two Numbers v2

--- src/main/java/com/leetcode/AddTwoNumbers.java
+++ src/main/java/com/leetcode/AddTwoNumbers.java
@@ -5,60 +5,22 @@ import com.leetcode.utils.ListNode;
 public class AddTwoNumbers {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        var curr1 = l1;
-        var curr2 = l2;
-        int reminder = 0;
-        var root = new ListNode();
-        var currRoot = root;
-        while (curr1 != null && curr2 != null) {
-            int localRes = curr1.val + curr2.val;
-            if (reminder > 0) {
-                localRes = localRes + reminder;
-            }
-            if (localRes / 10 > 0) {
-                reminder = localRes / 10;
-                currRoot.val = localRes - 10;
-            } else {
-                reminder = 0;
-                currRoot.val = localRes;
-            }
-            if (curr1.next != null && curr2.next != null) {
-                currRoot.next = new ListNode();
-                currRoot = currRoot.next;
-            }
-            curr1 = curr1.next;
-            curr2 = curr2.next;
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
         }
-        while (curr1 != null) {
-            currRoot.next = new ListNode();
-            currRoot = currRoot.next;
-            int localRes = curr1.val + reminder;
-            if (localRes / 10 > 0) {
-                reminder = localRes / 10;
-                currRoot.val = localRes - 10;
-            } else {
-                reminder = 0;
-                currRoot.val = localRes;
-            }
-            curr1 = curr1.next;
-        }
-        while (curr2 != null) {
-            currRoot.next = new ListNode();
-            currRoot = currRoot.next;
-            int localRes = curr2.val + reminder;
-            if (localRes / 10 > 0) {
-                reminder = localRes / 10;
-                currRoot.val = localRes - 10;
-            } else {
-                reminder = 0;
-                currRoot.val = localRes;
-            }
-            curr2 = curr2.next;
-        }
-        if (reminder > 0) {
-            currRoot.next = new ListNode(reminder);
-        }
-        return root;
+        return dummyHead.next;
     }
 
 }
